9e17d53752013ca27a1b4ccbbdf232c8c66bd80b
MatheusFF <matheusfreitasfernandes18@gmail.com>
2023-09-18 14:39:22 -0300

    Score e encerramento

--- Aula 02/code/Menu.py
+++ Aula 02/code/Menu.py
@@ -6,13 +6,13 @@ import pygame
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import WIN_WIDTH, C_ORANGE, MENU_OPTION, C_WHITE, C_YELLOW
+from code.Const import WIN_WIDTH, C_ORANGE, MENU_OPTION, C_WHITE, C_YELLOW, MENU_OPTION_SPACE
 
 
 class Menu:
     def __init__(self, window):
         self.window: Surface = window
-        self.surf = pygame.image.load('./asset/MenuBg.png')
+        self.surf = pygame.image.load('./asset/MenuBg.png').convert_alpha()
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self):
@@ -27,9 +27,9 @@ class Menu:
 
             for i in range(len(MENU_OPTION)):
                 if i == menu_option:
-                    self.menu_text(20, MENU_OPTION[i], C_YELLOW, (WIN_WIDTH / 2, 200 + 30 * i))
+                    self.menu_text(20, MENU_OPTION[i], C_YELLOW, (WIN_WIDTH / 2, MENU_OPTION_SPACE[i]))
                 else:
-                    self.menu_text(20, MENU_OPTION[i], C_WHITE, (WIN_WIDTH / 2, 200 + 30 * i))
+                    self.menu_text(20, MENU_OPTION[i], C_WHITE, (WIN_WIDTH / 2, MENU_OPTION_SPACE[i]))
             pygame.display.flip()
 
             # Verificar eventos
